a094323b6e1e0d973179b7fc2bdafb9e36b0fef8
YoungEun Park <63158239+YEPark745@users.noreply.github.com>
2021-02-15 10:40:59 -0500

    Add files via upload

---
+++ fileH3.java
@@ -0,0 +1,22 @@
+import java.io.File;
+import java.io.FileWriter; //Import the FileWriter class
+import java.io.IOException; //Import the IOException class to handle errors
+
+public class fileH3 {
+
+	public static void main(String[] args) {
+		try {
+			FileWriter myWriter=new FileWriter("fileName.txt");
+			myWriter.write("Files in Java might be tricky, but it is fun enough!");
+			
+			myWriter.close();
+			System.out.println("Successfully wrote to the file.");
+			
+		} catch(IOException e) {
+			System.out.println("An error occured.");
+			e.printStackTrace();
+		}
+
+	}
+
+}
